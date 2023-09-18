48f728d915bda3d6e40ce4a77e6cc9aec79473d3
adrianotav25 <adrianotpereira3@gmail.com>
2023-09-18 19:49:48 -0300

    Add project files.

---
+++ Gerenciamento De Biblioteca/FormAutores.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Gerenciamento_De_Biblioteca
+{
+    partial class FormAutores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNome = new TextBox();
+            txtNascionalidade = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            dtpDataNascimento = new DateTimePicker();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(50, 90);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(199, 23);
+            txtNome.TabIndex = 1;
+            // 
+            // txtNascionalidade
+            // 
+            txtNascionalidade.Location = new Point(50, 185);
+            txtNascionalidade.Name = "txtNascionalidade";
+            txtNascionalidade.Size = new Size(199, 23);
+            txtNascionalidade.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(52, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Nascionalidade";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(300, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Data de Nascimento";
+            // 
+            // dtpDataNascimento
+            // 
+            dtpDataNascimento.Location = new Point(300, 87);
+            dtpDataNascimento.Name = "dtpDataNascimento";
+            dtpDataNascimento.Size = new Size(200, 23);
+            dtpDataNascimento.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(350, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 24);
+            button1.TabIndex = 6;
+            button1.Text = "Cadastrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // FormAutores
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(513, 243);
+            Controls.Add(button1);
+            Controls.Add(dtpDataNascimento);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtNascionalidade);
+            Controls.Add(txtNome);
+            Controls.Add(label1);
+            Name = "FormAutores";
+            Text = "FormAutores";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNome;
+        private TextBox txtNascionalidade;
+        private Label label2;
+        private Label label3;
+        private DateTimePicker dtpDataNascimento;
+        private Button button1;
+    }
+}
---
+++ Gerenciamento De Biblioteca/FormLIvros.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Gerenciamento_De_Biblioteca
+{
+    partial class FormLIvros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Autor";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Genero";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(258, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 15);
+            label4.TabIndex = 3;
+            label4.Text = "ISBN";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(258, 42);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Ano de Publicação";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(27, 218);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(183, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(27, 137);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(27, 60);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(177, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(258, 57);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(236, 23);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(258, 137);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(235, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(401, 270);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Cadastrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // FormLIvros
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(506, 335);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormLIvros";
+            Text = "FormLIvros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
